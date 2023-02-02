0f9f6351bf3f0ad433a013629185d141c1b49436
Olimjon <olimjonsn230@gmail.com>
2023-02-02 18:04:02 +0500

    added tasks screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app_with_api/model/cardTask.dart';
+import 'package:todo_app_with_api/taskItem.dart';
 
 class HomeScreen extends StatelessWidget {
   static const routeName = '/homeScreen';
@@ -6,6 +8,59 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ),
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: const Icon(
+            Icons.menu,
+            color: Colors.black,
+          ),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(15),
+              child: Text(
+                'Lists',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            Expanded(
+              child: GridView.builder(
+                padding: const EdgeInsets.all(5),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 3 / 3,
+                ),
+                itemCount: 6,
+                itemBuilder: (BuildContext context, int index) {
+                  return taskItem(
+                    icon: cardTask[index]['icon'],
+                    index: index,
+                    title: cardTask[index]['title'],
+                    taskNumber: cardTask[index]['taskNumber'],
+                  );
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+              width: double.infinity,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,11 +1,98 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app_with_api/constants.dart';
+import 'package:todo_app_with_api/model/cardTask.dart';
 
-class Tasks extends StatelessWidget {
+class Tasks extends StatefulWidget {
   static const routeName = '/tasks';
-  const Tasks({super.key});
+  int index;
+  Tasks({required this.index, super.key});
+
+  @override
+  State<Tasks> createState() => _TasksState();
+}
+
+class _TasksState extends State<Tasks> {
+  bool checkbool = false;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back_ios_new_outlined),
+        ),
+        actions: const [
+          Icon(
+            Icons.more_vert,
+            size: 30,
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+              flex: 1,
+              child: Container(
+                color: Colors.blue,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(40.0),
+                      child: Column(
+                        children: [
+                          const Spacer(),
+                          CircleAvatar(
+                              radius: 40,
+                              backgroundColor: Colors.white,
+                              child: cardTask[widget.index]['icon']),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            cardTask[widget.index]['title'],
+                            style: titleStyleWhite,
+                          ),
+                          Text(
+                            '${cardTask[widget.index]['taskNumber']} tasks',
+                            style: tasksStyleWhite,
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              )),
+          Expanded(
+              flex: 2,
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(26),
+                      topRight: Radius.circular(26)),
+                ),
+                child: ListView.builder(
+                  itemCount: 20,
+                  itemBuilder: (BuildContext context, int index) {
+                    return CheckboxListTile(
+                        title: Text('$index'),
+                        value: checkbool,
+                        onChanged: ((value) {
+                          setState(() {
+                            value = checkbool ? true : false;
+                          });
+                        }));
+                  },
+                ),
+              )),
+        ],
+      ),
+    );
   }
 }
