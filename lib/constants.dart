205836e87b9adeb8a514be1a2bb9adc4c9758f35
Olimjon <olimjonsn230@gmail.com>
2023-02-02 18:04:28 +0500

    create constants

---
+++ lib/constants.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+TextStyle titleStyle = const TextStyle(
+  fontSize: 25,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle titleStyleWhite = const TextStyle(
+  fontSize: 30,
+  fontWeight: FontWeight.w600,
+  color: Colors.white,
+);
+TextStyle tasksStyle = const TextStyle(
+  color: Colors.grey,
+  fontSize: 15,
+);
+TextStyle tasksStyleWhite = const TextStyle(
+  color: Colors.white,
+  fontSize: 15,
+);
