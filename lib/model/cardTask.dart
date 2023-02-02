6a1a3444ad864cc2f4aadced026ef2b5ee455ef7
Olimjon <olimjonsn230@gmail.com>
2023-02-02 18:03:31 +0500

    model

---
+++ lib/model/cardTask.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+List cardTask = [
+  {
+    'icon': const Icon(
+      Icons.receipt_long_outlined,
+      color: Color(0xFF5786FF),
+      size: 35,
+    ),
+    'title': 'All',
+    'taskNumber': 2
+  },
+  {
+    'icon': const Icon(
+      Icons.work_outline,
+      color: Color(0xFFFDBF80),
+      size: 35,
+    ),
+    'title': 'Work',
+    'taskNumber': 2
+  },
+  {
+    'icon': const Icon(
+      Icons.library_music_outlined,
+      color: Color(0xFFF89885),
+      size: 35,
+    ),
+    'title': 'Music',
+    'taskNumber': 2
+  },
+  {
+    'icon': const Icon(
+      Icons.airplanemode_active_rounded,
+      color: Color(0xFF6AD18A),
+      size: 35,
+    ),
+    'title': 'Travel',
+    'taskNumber': 2
+  },
+  {
+    'icon': const Icon(
+      Icons.menu_book_rounded,
+      color: Color(0xFFB6B1E1),
+      size: 35,
+    ),
+    'title': 'Study',
+    'taskNumber': 2
+  },
+  {
+    'icon': const Icon(
+      Icons.home,
+      color: Color(0xFFF89885),
+      size: 35,
+    ),
+    'title': 'Home',
+    'taskNumber': 2
+  }
+];
